efaff2518c446a059554ab651de2db7e08d25f9e
Carlos Cunha <ccfontes@gmail.com>
2014-03-07 02:49:13 +0000

    Upgraded dependencies. Optimized .travis.yml
    Switched order of jdk tests in .travis.yml to privilege v7 because it is backwards compatible
    Cleaned project.clj and upgraded dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
 (defproject namban "0.1.5"
   :description "Convert text between different japanese scripts."
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-beta1"]]
-                   :plugins [[codox "0.6.6"] [lein-midje "3.1.2"] [lein-clojars "0.9.1"]]
-                   :codox {:sources ["src"]
-                           :output-dir "doc/api"}}}
-  :jvm-opts ["-Dfile.encoding=utf-8"]
-  :target-path "target"
-  :source-paths ["src"])
+  :profiles {:dev {:dependencies [[midje "1.6.2"]]
+                   :plugins [[codox "0.6.7"]
+                             [lein-midje "3.1.3"]
+                             [lein-clojars "0.9.1"]]}})
